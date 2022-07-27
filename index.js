ae944e56b91677938c62fe078864476234d0b419
panos-charalampopoulos <panos10cha@gmail.com>
2022-07-27 18:40:27 +0300

    add items to local storage

--- index.js
+++ index.js
@@ -10,25 +10,23 @@ window.onload = function () {
     cartCloseButton.addEventListener("click", function () {
         cartBox.classList.remove('active')
     });
-
-    // local storage
-
-    if (typeof (storage) !== 'undefined') {
-        console.log('working')
-    } else {
-        console.log('not working')
-    }
-    console.log(localStorage === window.localStorage);
-    localStorage.setItem('name', 'Tom');
-
-    console.log(localStorage.getItem('name'));
-
-    if (typeof window !== 'undefined') {
-        console.log('You are on the browser')
-        // 👉️ can use localStorage here
-      } else {
-        console.log('You are on the server')
-        // 👉️ can't use localStorage
-      }
 };
+
+// add data to local storage
+
+const addToCartButton = document.getElementsByClassName('product-grid__btn product-grid__add-to-cart');
+let items = [];
+for (let i = 0; i < addToCartButton.length; i++) {
+    addToCartButton[i].addEventListener('click', function (e) {
+        console.log(e.target.parentElement.children[0]);
+        if (typeof window !== 'undefined') {
+            let item = {
+                id: i + 1,
+                name: e.target.parentElement.children[0],
+            };
+} else {
+    alert('not working');
+}
+    });
+}
 
