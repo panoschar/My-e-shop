577cb50ab955997ebd26249a48a7ad687bccf777
panos-charalampopoulos <panos10cha@gmail.com>
2022-07-27 15:26:35 +0300

    local storage attempt

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-window.onload = function() {
-   const iconShopping = document.querySelector('.iconShopping');
-   const cartCloseButton = document.querySelector('.fa-close')
-   const cartBox = document.querySelector('.carBox');
+window.onload = function () {
+    const iconShopping = document.querySelector('.iconShopping');
+    const cartCloseButton = document.querySelector('.fa-close')
+    const cartBox = document.querySelector('.cartBox');
 
-   iconShopping.addEventListener("click", function() {
-       cartBox.classList.add('active');
-   });
-   cartCloseButton.addEventListener("click", function() {
-      cartBox.classList.remove('active');
-  });
-}
+    iconShopping.addEventListener("click", function () {
+        cartBox.classList.add('active')
+    });
+
+    cartCloseButton.addEventListener("click", function () {
+        cartBox.classList.remove('active')
+    });
+
+    // local storage
+
+    if (typeof (storage) !== 'undefined') {
+        console.log('working')
+    } else {
+        console.log('not working')
+    }
+    console.log(localStorage === window.localStorage);
+    localStorage.setItem('name', 'Tom');
+
+    console.log(localStorage.getItem('name'));
+
+    if (typeof window !== 'undefined') {
+        console.log('You are on the browser')
+        // 👉️ can use localStorage here
+      } else {
+        console.log('You are on the server')
+        // 👉️ can't use localStorage
+      }
+};
+
