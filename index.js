ab9d78d37cfd6b6dae9dcf88464ac0a43388edcf
panos-charalampopoulos <panos10cha@gmail.com>
2022-07-27 20:35:29 +0300

    local storage attempt

--- index.js
+++ index.js
@@ -12,18 +12,34 @@ window.onload = function () {
     });
 };
 
-// add data to local storage
+    // add data to local storage
 
 const addToCartButton = document.getElementsByClassName('product-grid__btn product-grid__add-to-cart');
 let items = [];
 for (let i = 0; i < addToCartButton.length; i++) {
     addToCartButton[i].addEventListener('click', function (e) {
-        console.log(e.target.parentElement.children[0]);
         if (typeof window !== 'undefined') {
             let item = {
                 id: i + 1,
-                name: e.target.parentElement.children[0],
+                name: e.target.parentElement.children[1].textContent,
+                price: e.target.parentElement.children[2].children[0].textContent,
+                no:1
             };
+            if(JSON.parse(localStorage.getItem('items')) === null) {
+                items.push(item);
+                localStorage.setItem("items",JSON.stringify(items));
+            }else {
+                const localItems = JSON.parse(localStorage.getItem("items"));
+                localItems.map(data => {
+                   if(item.id == data.id) {
+                    item.no += 1;
+                   }else {
+                    items.push(data);
+                   }
+                });
+                items.push(item);
+                localStorage.setItem('items',JSON.stringify(items));
+            }
 } else {
     alert('not working');
 }
